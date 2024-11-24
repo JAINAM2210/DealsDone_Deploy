6c66441c34906da0928dd0189e46b54e36b0327a
Jainam <jainam2210patel@gmail.com>
2024-11-24 05:33:42 +0530

    update  main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 // import dotenv from "dotenv";
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      {/* {dotenv.config()} */}
-      <App />
-    </StrictMode>
-  </Provider>
-)
+  React.createElement(Provider, { store },
+    React.createElement(StrictMode, null,
+      React.createElement(App, null)
+    )
+  )
+);
+
