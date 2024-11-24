0dfe20a1067dac26f0297e0157bfc8a057a45f35
Jainam <jainam2210patel@gmail.com>
2024-11-24 06:17:26 +0530

    update from index.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,44 @@
-import React from 'react' 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-// import dotenv from "dotenv";
+// import React from 'react' 
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// // import dotenv from "dotenv";
+// import { Provider } from 'react-redux';
+// import store from './redux/store.js';
+
+
+
+// createRoot(document.getElementById('root')).render(
+//   React.createElement(Provider, { store },
+//     React.createElement(StrictMode, null,
+//       React.createElement(App, null)
+
+
+//     )
+//   )
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import store from './redux/store';
 
-
-
-createRoot(document.getElementById('root')).render(
-  React.createElement(Provider, { store },
-    React.createElement(StrictMode, null,
-      React.createElement(App, null)
-
-
-    )
-  )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
 
